6b31b0d9847c5941a617073b506ac228b2b59f4e
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-10-27 11:08:13 +0100

    edits

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import "./App.css";
+import { createPortal } from "react-dom";
 import Search from "./components/search/Search";
 import { useState, useEffect } from "react";
 import ImageList from "./components/ImageList";
-import {unsplash} from './api/unsplash'
+import { unsplash } from "./api/unsplash";
 import Pagination from "@mui/material/Pagination";
 
 function App() {
   const [search, setSearch] = useState("love");
   const [images, setImages] = useState([]);
-  const[pages, setPages] = useState(0)
-  const [page, setPage] = useState(0)
+  const [pages, setPages] = useState(0);
+  const [page, setPage] = useState(0);
   const getSearchTerm = (term) => {
     setSearch(term);
   };
 
-  const handleChange = (event, value)=> {
+  const handleChange = (event, value) => {
     setPage(value);
-  }
-  
+  };
+
   useEffect(() => {
     const fetchPhotos = async () => {
-      const result = await unsplash.get(`/search/photos?query=${search}&per_page=34&page=${page}`);
+      const result = await unsplash.get(
+        `/search/photos?query=${search}&per_page=34&page=${page}`
+      );
       setImages(result.data.results);
-      setPages(result.data.total_pages)
+      setPages(result.data.total_pages);
     };
     fetchPhotos();
   }, [search, page]);
   return (
     <div className="App">
-      <Search getSearchTerm={getSearchTerm} />
+      {createPortal(
+        <Search getSearchTerm={getSearchTerm} />,
+        document.getElementById("search")
+      )}
+
       <ImageList images={images} />
       <Pagination
-      style={{
-        marginLeft: "40%",
-        marginBottom: "10px"
-       
-      }}
+        style={{
+          marginLeft: "40%",
+          marginBottom: "10px",
+        }}
         color="secondary"
         count={pages}
         page={page}
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,28 +1,19 @@
-
-
-import Stack from "@mui/material/Stack";
 import "./imageList.css"
 
 const ImageList = ({ images }) => {
   return (
-    <Stack spacing={2}>
       <div className="images">
         {images.map((image) => {
           return (
-            <div key={image.id}>
               <img
+                key={image.id}
                 className="img"
                 src={image.urls.small}
                 alt={image.alt_description}
               />
-            </div>
           );
         })}
       </div>
-     
-      
-    
-    </Stack>
   );
 };
 
